295bed617f1a96a259dcb5c5ca87245be2e4c1be
Krista <knk2110@columbia.edu>
2013-09-20 03:55:33 -0400

    added v3 players

---
+++ gunslinger/g8_v3/Player.java
@@ -0,0 +1,256 @@
+package gunslinger.g8;
+
+import java.util.*;
+import java.io.*;
+
+// Extends gunslinger.sim.Player to start with your player
+public class Player extends gunslinger.sim.Player
+{
+    // total versions of the same player
+    private static int versions = 0;
+    // my version no
+    private int version = versions++;
+
+    private double shootFirstEnemiesFactor = 5;
+
+    private double shootFirstFriendsFactor = 2;
+
+    private double isEnemyWeight = 1;
+
+    private double shotAtMeWeight = 2;
+
+    private double shotAtFriendWeight = 1;
+
+    private double shotAtEnemyWeight = 1;
+
+    private double roundDecayFactor = 0.9;
+
+    private double wasShotAtLastRoundFactor = 1;
+
+    private double weShotAtLastRoundFactor = 1.5;
+    
+    // name of the team
+    //
+    public String name()
+    {
+        return "g8";
+    }
+
+    public void setConfiguration(String config) {
+	shootFirstEnemiesFactor = convertConfig(config.charAt(0), 1);
+	shootFirstFriendsFactor = convertConfig(config.charAt(1), 2);
+	isEnemyWeight = convertConfig(config.charAt(2), 0);
+	shotAtMeWeight = convertConfig(config.charAt(3), 0);
+	shotAtFriendWeight = convertConfig(config.charAt(4), 0);
+	shotAtEnemyWeight = convertConfig(config.charAt(5), 0);
+	roundDecayFactor = convertConfig(config.charAt(6), 3);
+	wasShotAtLastRoundFactor = convertConfig(config.charAt(7), 0);
+	weShotAtLastRoundFactor = convertConfig(config.charAt(8), 0);	
+    }
+
+    public double convertConfig(int value, int mode) {
+	switch (value) {
+	case '0':
+	    switch (mode) {
+	    case 0:
+		return 1;
+	    case 1:
+		return 5;
+	    case 2:
+		return 2;
+	    case 3:
+		return 0.9;
+	    }
+	case '1':
+	    switch (mode) {
+	    case 0:
+		return 1.5;
+	    case 1:
+		return 5.5;
+	    case 2:
+		return 2.5;
+	    case 3:
+		return 0.8;
+	    }
+	case '2':
+	    switch (mode) {
+	    case 0:
+		return 2;
+	    case 1:
+		return 6;
+	    case 2:
+		return 3;
+	    case 3:
+		return 0.7;
+	    }
+	}
+	return 0;
+    }
+
+    public void loadConstants() {
+	String csv = "constants.csv";
+	BufferedReader br = null;
+	String line = "";
+	String csvSplit = ",";
+
+	try {
+	    br = new BufferedReader(new FileReader(csv));
+	    while ((line = br.readLine()) != null) {
+		String[] constants = line.split(csvSplit);
+		shootFirstEnemiesFactor = Double.parseDouble(constants[0]);
+		shootFirstFriendsFactor = Double.parseDouble(constants[1]);
+		isEnemyWeight = Double.parseDouble(constants[2]);
+		shotAtMeWeight = Double.parseDouble(constants[3]);
+		shotAtFriendWeight = Double.parseDouble(constants[4]);
+		shotAtEnemyWeight = Double.parseDouble(constants[5]);
+		roundDecayFactor = Double.parseDouble(constants[6]);
+		wasShotAtLastRoundFactor = Double.parseDouble(constants[7]);
+		weShotAtLastRoundFactor = Double.parseDouble(constants[8]);
+	    }
+	} catch (FileNotFoundException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	} finally {
+	    if (br != null) {
+		try {
+		    br.close();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	    }
+	}
+    }
+ 
+    // Initialize the player
+    //
+    public void init(int nplayers, int[] friends, int enemies[])
+    {
+        this.nplayers = nplayers;
+        this.friends = new Integer[friends.length];
+        for (int i = 0; i < friends.length; i++) {
+            this.friends[i] = friends[i];
+        }
+        this.enemies = new Integer[enemies.length];
+        for (int i = 0; i < enemies.length; i++) {
+            this.enemies[i] = enemies[i];
+        }
+    	this.history = new List[nplayers];
+        this.timesTargeted = new Integer[nplayers];
+    	for (int i = 0; i < nplayers; i++) {
+    	    history[i] = new ArrayList<Integer>();
+            timesTargeted[i] = 0;
+    	}
+	previousTarget = -1;
+    }
+
+    // Pick a target to shoot
+    // Parameters:
+    //  prevRound - an array of previous shoots, prevRound[i] is the player that player i shot
+    //              -1 if player i did not shoot
+    //  alive - an array of player's status, true if the player is still alive in this round
+    // Return:
+    //  int - the player id to shoot, return -1 if do not shoot anyone
+    //
+    public int shoot(int[] prevRound, boolean[] alive)
+    {
+    	if (prevRound != null) {
+      	    for (int i = 0; i < nplayers; i++) {    		
+        	history[i].add(0, prevRound[i]);
+                if (prevRound[i] != -1) {
+                    timesTargeted[prevRound[i]]++;
+                }
+    	    }
+    	} else if (enemies.length * shootFirstEnemiesFactor < nplayers || friends.length * shootFirstFriendsFactor > nplayers) {
+	    return -1;
+	}
+	double maxRank = 0;
+	int target = -1;
+        for (int i = 0; i != nplayers; ++i) {
+            if (i != id && alive[i] && !isFriend(i)) {
+		double tempRank = rank(alive, prevRound, i);
+                if (maxRank < tempRank) {
+		    maxRank = tempRank;
+		    target = i;
+		}
+	    }
+	    /*
+	    if (Thread.currentThread().isInterrupted()) {
+		previousTarget = target;
+		return target;
+		} */
+	}    	
+	previousTarget = target;
+        return target;
+    }
+    
+    public List<Integer> shotAtLastRound(int[] prevRound, boolean[] alive) {
+	List<Integer> shotAtLastRound = new ArrayList<Integer>();
+	if (prevRound == null) {
+	    return shotAtLastRound;
+	}
+	for (int i = 0; i < nplayers; i++) {
+	    if (prevRound[i] != -1 && alive[i] && alive[prevRound[i]] && isFriend(i)
+		      && !shotAtLastRound.contains(prevRound[i])) {
+		    shotAtLastRound.add(prevRound[i]);
+	    }
+	}
+	return shotAtLastRound;
+    }
+
+    // Assigns a ranking to a player. Higher ranking means a higher priority to shoot
+    // Parameters:
+    //  alive - an array of player's status, true if the player is still alive in this round
+    //  target - the id of the person we are trying to assign a ranking to
+    // Rerturn:
+    // int - the ranking for the target
+    public double rank(boolean[] alive, int[] prevRound, int target) {
+    	double rank = isEnemy(target) ? isEnemyWeight : 0;        
+    	List<Integer> playerHistory = history[target];
+	List<Integer> shotAtLastRound = shotAtLastRound(prevRound, alive);
+	double roundWeight = 1;
+    	for (int otherTarget : playerHistory) {
+    	    if (id == otherTarget) {
+    		  rank += shotAtMeWeight;
+    	    } else if (isFriend(otherTarget) && alive[otherTarget]) {
+		rank += shotAtFriendWeight;
+    	    } else if (isEnemy(otherTarget) && alive[otherTarget]) {
+		rank -= shotAtEnemyWeight;
+    	    }
+	    roundWeight *= roundDecayFactor;
+    	}
+	if (shotAtLastRound.contains(target) &&
+	    ((isNeutral(target) && enemies.length < friends.length + 1) || isEnemy(target))) {
+	    rank += wasShotAtLastRoundFactor;
+	}
+	if (target == previousTarget && alive[previousTarget]) {
+	    rank+= weShotAtLastRoundFactor;
+	}
+    	return rank;
+    }
+
+    public boolean isEnemy(int id) {
+	return Arrays.asList(enemies).contains(id);
+    }
+
+    public boolean isFriend(int id) {
+	return Arrays.asList(friends).contains(id);
+    }
+
+    public boolean isNeutral(int id) {
+	return !isEnemy(id) && !isFriend(id);
+    }
+
+    // player we last shot at
+    private int previousTarget;
+    // number of players in the game
+    private int nplayers;
+    // list of ids that correspond to friends
+    private Integer[] friends;
+    // list of ids that correspond to enemies
+    private Integer[] enemies;
+    // array of Lists that represent the previous moves of the other players
+    private List<Integer>[] history;
+    // list that represents the number of times each player has been targeted
+    private Integer[] timesTargeted;
+}
